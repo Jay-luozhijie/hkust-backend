e405eb8c1eba690c95a5619058c73e85f344c6aa
Jay <jayluo36@gmail.com>
2024-07-22 18:47:33 +0800

    add deletion feature

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -25,15 +25,11 @@ userRouter.post('/login', expressAsyncHandler(async(req, res) =>{
 }))
 
 userRouter.post('/saveNews', expressAsyncHandler(async(req, res) =>{
-
-    console.log('saveNews')
     
     const num_of_news = await News.countDocuments();
     const id = num_of_news + 1;
     const content = req.body.content;
     const title = req.body.title;
-    console.log(content)
-    console.log(title)
     try {
         const newsItem = new News({ _id: id, title: title, content:content });
         await newsItem.save();
@@ -46,16 +42,11 @@ userRouter.post('/saveNews', expressAsyncHandler(async(req, res) =>{
 
 
 userRouter.post('/saveAlumniAct', expressAsyncHandler(async(req, res) =>{
-
-    console.log('hello')
-    
     const num_of_news = await AlumniActivities.countDocuments();
     const id = num_of_news + 1;
     const content = req.body.content;
     const title = req.body.title;
     const image_url = req.body.img_url
-    console.log(content)
-    console.log(title)
     try {
         const newsItem = new AlumniActivities({ _id: id, title: title, content:content, img_url:image_url });
         await newsItem.save();
@@ -66,27 +57,57 @@ userRouter.post('/saveAlumniAct', expressAsyncHandler(async(req, res) =>{
     }
 }))
 
+userRouter.delete('/news/:id', async (req, res) => {
+    try {
+        const newsItem = await News.findByIdAndDelete(req.params.id);
+        if (!newsItem) {
+            return res.status(404).json({ error: 'News item not found' });
+        }
+        res.status(204).send(); // No Content
+    } catch (error) {
+        console.error('Error deleting news:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 userRouter.get('/news', async (req, res) => {
-    const { page = 1, limit = 4 } = req.query;
-    console.log(page)
-    console.log(limit)
+    const { page = 1, limit = 4, isAll = 0 } = req.query;
     try {
-      const news = await News.find()
-        .sort({ updatedAt: -1 })
-        .skip((page - 1) * limit)
-        .limit(parseInt(limit));
-      res.json(news);
+        if (parseInt(isAll) == 1){
+            const news = await News.find()
+            .sort({ updatedAt: -1 });
+            res.json(news);
+        } else{
+            const news = await News.find()
+            .sort({ updatedAt: -1 })
+            .skip((page - 1) * limit)
+            .limit(parseInt(limit));
+            res.json(news);
+        }
     } catch (error) {
       res.status(500).json({ message: 'Error fetching news data', error });
     }
   });
 
+userRouter.delete('/AlumniAct/:id', async (req, res) => {
+    try {
+        const newsItem = await AlumniActivities.findByIdAndDelete(req.params.id);
+        if (!newsItem) {
+            return res.status(404).json({ error: 'News item not found' });
+        }
+        res.status(204).send(); // No Content
+    } catch (error) {
+        console.error('Error deleting news:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
 userRouter.get('/AlumniAct', async (req, res) => {
-    const { page = 1, limit = 4, fetchAll = 0 } = req.query;
+    const { page = 1, limit = 4, isAll = 0 } = req.query;
 
     try {
-        if (parseInt(fetchAll) === 1){
+        if (parseInt(isAll) === 1){
             let news = await AlumniActivities.find().sort({ updatedAt: -1 })
             res.json(news);
         } else {
@@ -111,7 +132,6 @@ userRouter.get('/newsDetail', async (req, res) => {
     try {
         const news = await News.findById(id);
     if (!news) {
-        console.log('News not found');
         // Handle case where news with given ID is not found
     } else {
         res.json(news);
@@ -123,7 +143,6 @@ userRouter.get('/newsDetail', async (req, res) => {
 
 userRouter.get('/activitiesDetail', async (req, res) => {
     const { id = 1 } = req.query;
-    console.log('hello')
     
     try {
         const news = await AlumniActivities.findById(id);
